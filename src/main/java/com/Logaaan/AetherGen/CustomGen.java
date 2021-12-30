1f1a54589d5243b5c9ef28de4a123215c06a2fa2
Shane Parslow <shaneparslow808@gmail.com>
2021-12-30 00:50:17 -0800

    Switch to generateNoise

--- src/main/java/com/Logaaan/AetherGen/CustomGen.java
+++ src/main/java/com/Logaaan/AetherGen/CustomGen.java
@@ -5,6 +5,7 @@ import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,20 +16,10 @@ import java.util.Random;
 public class CustomGen extends ChunkGenerator {
     public Main p;
     public String wn;
-    SimplexOctaveGenerator generator_float;
-    private boolean i = false;
 
     public CustomGen(String wn, Main m) {
         p = m;
         this.wn = wn;
-    }
-
-    private void init(World world) {
-        // Is this unique for each world created?
-        // In other words, is a CustomGen created for each world created, or only on first startup?
-        generator_float = new SimplexOctaveGenerator(world, p.ao);
-        generator_float.setScale(0.005);
-        i = true;
     }
 
     @Override
@@ -37,50 +28,59 @@ public class CustomGen extends ChunkGenerator {
         return Arrays.asList((BlockPopulator) new PopulatorOre(p));
     }
 
+    // Yeah, this basically puts all the world generation in a single function.
+    // I could split it up by making an unordered dynamic queue thing indexed by chunk coords,
+    // and filling that with the noise. That would be super complicated for no reason though.
+    // TODO: add slight noise to lower island bound, so that it isn't an exact mirror
+    // TODO: * 7D can maybe be replaced by changing p.a
     @Override
-    public @NotNull ChunkData generateChunkData(@NotNull World world, @NotNull Random random, int chunkX, int chunkZ, @NotNull BiomeGrid biome) {
-        if (!i) {
-            init(world);
-        }
+    public void generateNoise(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkGenerator.ChunkData chunkData) {
+        SimplexOctaveGenerator gen = new SimplexOctaveGenerator(new Random(worldInfo.getSeed()), p.ao);
+        SimplexOctaveGenerator lower_gen = new SimplexOctaveGenerator(new Random(worldInfo.getSeed()+1), p.ao);
+        // This can potentially be eliminated by changing generator frequency
+        gen.setScale(0.005);
+        lower_gen.setScale(0.005);
 
-        // World coordinate of the 0,0 block in the chunk being generated
         int worldBaseX = chunkX * 16;
         int worldBaseZ = chunkZ * 16;
 
-        ChunkData chunk = createChunkData(world);
-
         for (int X = 0; X < 16; X++) {
             for (int Z = 0; Z < 16; Z++) {
-                // World coordinate of current block
                 int worldX = worldBaseX + X;
                 int worldZ = worldBaseZ + Z;
 
-                Biome b = world.getBiome(worldX, worldZ);
-
-                double noise = generator_float.noise(worldX, worldZ, p.f, p.a);
+                double noise = gen.noise(worldX, worldZ, p.f, p.a);
                 int currentHeight = (int) (noise * 7D + p.ah);
+                // A small offset to the lower bound of islands, in order to make sure they are not perfectly mirrored.
+                double offset_noise = lower_gen.noise(worldX, worldZ, p.f, p.a);
+                int lower_offset = (int) (offset_noise * 7D);
+                int lower_y = (int) (p.ah - (noise * 7D - 13D) + lower_offset);
 
-                for (int Y = currentHeight; Y > p.ah - (noise * 7D - 13D); Y--) {
+                for (int Y = currentHeight; Y > lower_y; Y--) {
+                    Biome biome = chunkData.getBiome(X, Y, Z);
                     if (noise > p.sp) {
                         // On top block
                         if (Y == currentHeight) {
-                            if (world.getBiome(worldX, worldZ).equals(Biome.DESERT)) {
-                                chunk.setBlock(X, Y, Z, Material.SAND);
+                            if (biome.equals(Biome.DESERT)) {
+                                chunkData.setBlock(X, Y, Z, Material.SAND);
                             } else {
-                                chunk.setBlock(X, Y, Z, Material.GRASS_BLOCK);
-
+                                chunkData.setBlock(X, Y, Z, Material.GRASS_BLOCK);
                             }
                         // On second to top block
                         } else if (Y == currentHeight - 1) {
-                            chunk.setBlock(X, Y, Z, Material.DIRT);
+                            chunkData.setBlock(X, Y, Z, Material.DIRT);
                         // On some other block, for loop ensures within sane y limits
                         } else {
-                            chunk.setBlock(X, Y, Z, new Random().nextBoolean() ? Material.COBBLESTONE : Material.STONE);
+                            chunkData.setBlock(X, Y, Z, random.nextBoolean() ? Material.COBBLESTONE : Material.STONE);
                         }
                     }
                 }
             }
         }
-        return chunk;
+    }
+
+    @Override
+    public boolean shouldGenerateCaves() {
+        return true;
     }
 }
